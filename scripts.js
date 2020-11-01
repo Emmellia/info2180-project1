27af14304c75905c932a02fb1808ee9f75424f30
Emmellia Hayden <emmelliahayden@gmail.com>
2020-11-01 15:33:47 -0500

    Edited files

--- scripts.js
+++ scripts.js
@@ -1,40 +1,20 @@
 /* Add your JavaScript to this file */
 "use strict";
 window.onload=function(){
-    let elements=document.getElementsByClassName("newsletter");
     let subscribeBtn=document.getElementsByTagName("button")[0];
     let message=document.getElementsByClassName("message")[0];
+    let emailField=document.getElementById("email");
 
     subscribeBtn.addEventListener("click", function(e){
         e.preventDefault();
-        let email=document.getElementById("email").value;
+        let email=emailField.value;
         if(email===""){
-            message.textContent="Please enter a valid email address";
-        }
+            message.textContent="Please enter a valid email address";  }
 
-        else{message.textContent= `Thank you! Your email address ${email} has been added to our mailing list!`;}
-
-        console.log(message);
+        else{message.textContent= `Thank you! Your email address ${email} has been added to our mailing list!`;
+            emailField.value= " ";
+                }
     
     })
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
